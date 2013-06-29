e1f49876f01a45a233de051775d71c8685667537
allen <allen@b4c2425b-e095-0cc1-4a97-0096e35b8b1a>
2013-06-29 03:47:39 +0000

    Checking in temporary zsq code for the road.

--- src/headers/dwi_read.h
+++ src/headers/dwi_read.h
@@ -23,4 +23,12 @@ int readDWICenter(std::vector<struct ARROW> *chart, std::vector<struct FREEZE> *
 // postcondition: calls readDWI, then moves columns 0-3 to 4-7
 // returns: the maximum score in dance points for this chart
 
+int readXSQ(std::vector<struct ARROW> *chart, std::vector<struct FREEZE> *holds, int songID, int chartType);
+int readXSQ2P(std::vector<struct ARROW> *chart, std::vector<struct FREEZE> *holds, int songID, int chartType);
+int readXSQCenter(std::vector<struct ARROW> *chart, std::vector<struct FREEZE> *holds, int songID, int chartType);
+// precondition: called only when the matching readDWI() function fails
+// postcondition: tries to load a similar .xsq file and make a chart that data
+// NOTE: should only be called from readDWI()
+
+
 #endif
