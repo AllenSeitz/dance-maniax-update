93b1857bda33fca21f4c9cc6d522e9bab050ff50
allen <allen@b4c2425b-e095-0cc1-4a97-0096e35b8b1a>
2013-09-14 01:33:14 +0000

    New "new songs" feature for the attract loop.

--- src/source/attractMode.cpp
+++ src/source/attractMode.cpp
@@ -35,6 +35,7 @@ UTIME submodeTimer = 0;
 
 int hitlist[20];
 int numplays[20];
+int newlist[10];
 
 char versionString[128] = "";
 
@@ -44,11 +45,19 @@ BITMAP* m_logo = NULL;
 BITMAP* m_mask = NULL;
 BITMAP* m_hitchart = NULL;
 
+const int NUM_ATTRACT_SUBMODES = 5;
+
 void advanceToNextMode()
 {
-	attractState = (attractState + 1) % 4;
+	attractState = (attractState + 1) % NUM_ATTRACT_SUBMODES;
 	attractSubState = submodeTimer = 0;
 	hideCreditsTimer = 1000;
+
+	// if nothing is new, then skip the newlist step
+	if ( attractState == 2 && newlist[0] == -1 )
+	{
+		attractState = 3;
+	}
 
 	switch ( attractState )
 	{
@@ -59,16 +68,17 @@ void advanceToNextMode()
 	case 1: // title
 		vm.stop();
 		break;
-	case 2: // how to
+	case 2: // new songs
+		break;
+	case 3: // how to
 		vm.loadScript("DATA/mov/A_HOWTO.seq");
 		vm.play();
 		break;
-	case 3: // hitchart
+	case 4: // hitchart
 		vm.stop();
 		break;
 	}
 }
-
 
 void calculateTop20()
 {
@@ -104,6 +114,40 @@ void calculateTop20()
 	}
 }
 
+void calculateNewSongs()
+{
+	int i = 0, j = 0;
+
+	for ( i = 0; i < 10; i++ )
+	{
+		newlist[i] = -1;
+	}
+
+	for ( int i = 0; i < NUM_SONGS; i++ )
+	{
+		if ( songs[i].isNew && songs[i].version != 0 && songs[i].version != 100 )
+		{
+			newlist[j] = songs[i].songID;
+			j++;
+
+			if ( j == 10 )
+			{
+				break; // that many new songs shouldn't happen
+			}
+		}
+	}
+}
+
+void renderHitlistBackground()
+{
+	int scroll = (submodeTimer/20) % 96;
+	for ( int x = 0; x < 9; x++ )
+	for ( int y = 0; y < 7; y++ )
+	{
+		blit(m_tile, rm.m_backbuf, 0, 0, (-96+scroll) + x*96, (-96+scroll) + y*96, 96, 96);
+	}
+}
+
 void firstAttractLoop()
 {
 	if ( m_title == NULL )
@@ -118,6 +162,7 @@ void firstAttractLoop()
 	}
 
 	calculateTop20();
+	calculateNewSongs();
 	attractSubState = submodeTimer = 0;
 	advanceToNextMode();
 	gs.killSong(); // when time runs out on the main menu
@@ -183,20 +228,76 @@ void mainAttractLoop(UTIME dt)
 			advanceToNextMode();
 		}
 		break;
-	case 2: // how to play
+	case 2: // new songs
+		renderHitlistBackground();
+
+		if ( attractSubState > 9 || newlist[attractSubState] == -1 )
+		{
+			advanceToNextMode();
+		}
+		else
+		{
+			int thisBannerTime = submodeTimer % 5000;
+			int thisBannerState = thisBannerTime > 1000 ? 1 : 0;
+
+			int bannerIndex = songID_to_listID(newlist[attractSubState]);
+			bool isCourse = songs[bannerIndex].version == 101;
+			if ( bannerIndex == -1 )
+			{
+				//songIndexError(newlist[attractSubState]);
+				advanceToNextMode(); // do this instead, or else the machine will be stuck on the error screen (if it ever happens)
+			}
+
+			// zoom in
+			if ( thisBannerState == 0 )
+			{
+				int width = getValueFromRange(512, 256, thisBannerTime*100/1000);
+				stretch_blit(m_banners[bannerIndex], rm.m_backbuf, 0, 0, 256, 256, (SCREEN_WIDTH/2)-(width/2), (SCREEN_HEIGHT/2)-(width/2), width, width);
+			}
+			// land and text
+			else
+			{
+				int x = (SCREEN_WIDTH/2)-128;
+				int y = (SCREEN_HEIGHT/2)-128;
+
+				// apply brightness flicker
+				if ( (thisBannerTime / 250) % 2 == 0 )
+				{
+					set_luminance_blender(256, 256, 256, 256);
+				}
+				stretch_blit(m_banners[bannerIndex], rm.m_backbuf, 0, 0, 256, 256, x, y, 256, 256);
+				set_alpha_blender(); // the game assumes the graphics are left in this mode
+
+				int color = (submodeTimer/50) % 3 + 1;
+				renderBoldString( isCourse ? "NEW NONSTOP COURSE!" : "NEW SONG!", x, y-48, 400, false, color);
+
+				if ( thisBannerTime > 1125 )
+				{
+					renderBoldString(songTitles[bannerIndex].c_str(), x, y +256+32, 400, false, 0);
+				}
+				if ( thisBannerTime > 1375 )
+				{
+					renderArtistString(songArtists[bannerIndex].c_str(), x, y +256+64, 400, 32);
+				}
+			}
+
+			// check for advance to next banner
+			if ( submodeTimer >= 5000*(attractSubState+1) )
+			{
+				attractSubState++;
+			}
+		}
+
+		break;
+	case 3: // how to play
 		vm.renderToSurfaceStretched(rm.m_backbuf, 0, 0, 640, 460);
 		if ( submodeTimer > 17000 )
 		{
 			advanceToNextMode();
 		}
 		break;
-	case 3: // hit chart
-		int scroll = (submodeTimer/20) % 96;
-		for ( int x = 0; x < 9; x++ )
-		for ( int y = 0; y < 7; y++ )
-		{
-			blit(m_tile, rm.m_backbuf, 0, 0, (-96+scroll) + x*96, (-96+scroll) + y*96, 96, 96);
-		}
+	case 4: // hit chart
+		renderHitlistBackground();
 		masked_blit(m_hitchart, rm.m_backbuf, 0, 0, 34, 32, 573, 34);
 
 		// render banners and stuff for each of the top 20
@@ -229,7 +330,8 @@ void mainAttractLoop(UTIME dt)
 			int bannerIndex = songID_to_listID(hitlist[i]);
 			if ( bannerIndex == -1 )
 			{
-				songIndexError(hitlist[i]);
+				//songIndexError(hitlist[i]);
+				advanceToNextMode(); // do this instead, or else the machine will be stuck on the error screen (if it ever happens)
 			}
 			stretch_blit(m_banners[bannerIndex], rm.m_backbuf, 0, 0, 256, 256, x, y + 11, 128, 128);
 			int color = i % 2;
