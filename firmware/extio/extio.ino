2e9d3cfa615d5f161420faccdc5dbb313a28944f
allen <allen@b4c2425b-e095-0cc1-4a97-0096e35b8b1a>
2013-11-23 03:18:58 +0000

    Placeholder extio sketch for an Arduino Mega. Completes a handshake with the game and then manipulates an ASCII string as a proof of concept.

---
+++ firmware/extio/extio.ino
@@ -0,0 +1,64 @@
+/* comment */
+
+String inputBuffer;
+String outputBuffer;
+
+boolean foundSoftware = false;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  // reply to any messages sent
+  if ( outputBuffer.length() > 0 )
+  {
+    Serial.print(outputBuffer);
+    outputBuffer = String();
+  }
+  
+  // video amp
+  
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    // keep listening until an entire packet (3 bytes) is read
+    char inChar = (char)Serial.read(); 
+    inputBuffer += inChar;
+
+    // process that packet
+    if ( inputBuffer.length() >= 3 )
+    {
+      if (inputBuffer == "DMX") 
+      {
+        outputBuffer = String("OK!");
+        foundSoftware = true;
+      }
+      else if ( inputBuffer.startsWith("PIN") )
+      {
+        outputBuffer = String("PON");
+      }
+      else
+      {
+        //outputBuffer = inputBuffer;
+        outputBuffer = String("000");
+        outputBuffer[0] = inputBuffer[0] + 1;
+        outputBuffer[1] = inputBuffer[1] + 1;
+        outputBuffer[2] = inputBuffer[2] + 1;
+      }
+      
+      inputBuffer = String();
+    }
+  }
+}
